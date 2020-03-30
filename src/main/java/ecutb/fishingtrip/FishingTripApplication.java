6c172fb29b47ad1a06c02de72b858f51fe16048b
oscrj <oscar.johanneson@gmail.com>
2020-03-30 13:48:32 +0200

    SetUp

---
+++ src/main/java/ecutb/fishingtrip/FishingTripApplication.java
@@ -0,0 +1,13 @@
+package ecutb.fishingtrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishingTripApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FishingTripApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ecutb/fishingtrip/FishingTripApplicationTests.java
@@ -0,0 +1,13 @@
+package ecutb.fishingtrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishingTripApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
