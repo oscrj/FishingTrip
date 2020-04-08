b8ddd036f448447cfca0aca4f47b25477d1ec8e0
Oscar Johanneson <oscar.johanneson@gmail.com>
2020-04-08 15:07:30 +0200

    Created users-view for ADMINS.

--- src/main/java/ecutb/fishingtrip/controller/AdminController.java
+++ src/main/java/ecutb/fishingtrip/controller/AdminController.java
@@ -1,11 +1,15 @@
 package ecutb.fishingtrip.controller;
 
+import ecutb.fishingtrip.entity.AppUser;
 import ecutb.fishingtrip.service.AppUserService;
 import ecutb.fishingtrip.service.FishingTripService;
 import ecutb.fishingtrip.service.SpeciesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class AdminController {
@@ -22,8 +26,9 @@ public class AdminController {
     }
 
     @GetMapping("admin/users")
-    public String getAllUsers(){
-
+    public String getAllUsers(Model model){
+        List<AppUser> users = userService.findAll();
+        model.addAttribute("users", users);
         return "users-view";
     }
 }
